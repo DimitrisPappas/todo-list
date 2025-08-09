337f8989491ccbd0bbad99d07fcf23b279cb01c4
DimitrisPappas <pappasdimitrisece@gmail.com>
2025-08-09 18:17:20 +0300

    Add task mapper

---
+++ src/main/kotlin/com/dspappas/todo_list/task/TaskMapper.kt
@@ -0,0 +1,34 @@
+package com.dspappas.todo_list.task
+
+import com.dspappas.todo_list.task.dtos.TaskResponse
+import org.springframework.stereotype.Component
+
+@Component
+class TaskMapper() {
+
+    fun fromRequestToEntity(
+        name: String,
+        isCompleted: Boolean
+    ): Task {
+        return Task(
+            name = name,
+            isCompleted = isCompleted
+        )
+    }
+
+    fun fromEntityToResponse(
+        entity: Task
+    ): TaskResponse {
+        return TaskResponse(
+            id = entity.id,
+            name = entity.name,
+            isCompleted = entity.isCompleted
+        )
+    }
+
+    fun fromEntityListToResponseList(
+        entityList: List<Task>
+    ) : List<TaskResponse> {
+        return entityList.map { e -> fromEntityToResponse(e) }
+    }
+}
